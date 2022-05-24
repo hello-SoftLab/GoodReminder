24e73ab45367ceec7e451a5f3d7d922b47200eb1
knz13 <otaviomaya@gmail.com>
2022-05-24 03:10:33 -0300

    removing breaking cflags

--- app/src/main/cpp/sources/program_stages/loading_stage.cpp
+++ app/src/main/cpp/sources/program_stages/loading_stage.cpp
@@ -1,16 +1,30 @@
 #include "loading_stage.h"
+#include <fstream>
 
 void LoadingStage::Init() {
+    std::string path = "";
 
 
+    path += "data/data/knz.goodreminder/";
+    path += "logo_alletercaseoff.png";
+    //m_Texture = LoadTextureFromFile<Type2D>( path);
+
+    if(!std::filesystem::exists(path)){
+        std::fstream stream;
+        stream.open(path);
+        stream << "i";
+        stream.close();
+    }
 
 }
 
 void LoadingStage::Update(float deltaTime) {
 
 
-    
+    if(m_Texture){
+        ImGui::Image((void*)m_Texture.GetID(),ImVec2(100,100));
 
+    }
 
     animationTime += deltaTime;
 
